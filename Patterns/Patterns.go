5a547a50e283875157be8f3a04bd223e843d8807
David Tauraso <david318@me.com>
2024-01-09 21:06:34 -0800

    Updated code

--- Patterns/Patterns.go
+++ Patterns/Patterns.go
@@ -267,12 +267,14 @@ var catagoryTracker = map[int]CategoryTracker{}
 func (sh *SequenceHierarchy) Categorize() {
 
 	// trackingDict := map[int]CategoryTracker{}
+	nodeIdMatches := map[int]int{}
 	functionNameCurrentOccurrenceCount := map[string]int{}
 	newSequenceIdTracker := sh.FirstNodeIdLastSequenceAdded
-
+	sizeOfNewSequence := 0
 	for ; newSequenceIdTracker != -1; newSequenceIdTracker = (*sh.Sequences)[newSequenceIdTracker].GetLastEdge("next") {
 
 		functionNameNewSequence := (*sh.Sequences)[newSequenceIdTracker].FunctionName
+		// prevents 1 occurrence from connecting to more than 1 occurrences in previously saved sequences
 		if _, isOccurrenceRecord := functionNameCurrentOccurrenceCount[functionNameNewSequence]; !isOccurrenceRecord {
 			functionNameCurrentOccurrenceCount[functionNameNewSequence] = 1
 		} else {
@@ -283,8 +285,98 @@ func (sh *SequenceHierarchy) Categorize() {
 			if _, isNodeIdInNewSequence := sh.NodeIdsLastSequenceAdded[nodeId]; isNodeIdInNewSequence {
 				continue
 			}
-			if occurrenceCount > functionNameCurrentOccurrenceCount[functionNameNewSequence] {
+			if occurrenceCount != functionNameCurrentOccurrenceCount[functionNameNewSequence] {
 				continue
+			}
+			// nodeId node matches with newSequenceIdTracker node
+			nodeIdMatches[nodeId] = newSequenceIdTracker
+		}
+		sizeOfNewSequence += 1
+	}
+	fmt.Printf("%v\n", nodeIdMatches)
+	x := map[int]struct{}{}
+	for key := range nodeIdMatches {
+		x[key] = struct{}{}
+	}
+	visited := x
+
+	advancedCount := 1
+	for advancedCount > 0 {
+		advancedCount = 0
+		y := map[int]struct{}{}
+		for nodeId := range x {
+			nextNodeId := (*sh.Sequences)[nodeId].GetLastEdge("next")
+			// skip over if at end of sequence
+			if nextNodeId == -1 {
+				continue
+			}
+			// skip over if node id has already been visited
+			if _, ok := visited[nextNodeId]; ok {
+				continue
+			}
+			visited[nextNodeId] = struct{}{}
+			advancedCount += 1
+			y[nextNodeId] = struct{}{}
+		}
+
+		if advancedCount > 0 {
+			x = y
+		}
+	}
+	fmt.Printf("%v\n", x)
+
+	nodeIdSize := map[int]int{}
+	for nodeId, _ := range x {
+		nodeIdSize[nodeId] = 1
+	}
+	atBeginingCount := 0
+	nodeIdSizeLength := len(nodeIdSize)
+	for atBeginingCount < nodeIdSizeLength {
+		for nodeId := range nodeIdSize {
+			prevNodeId := (*sh.Sequences)[nodeId].GetLastEdge("prev")
+			if prevNodeId == -1 {
+				atBeginingCount += 1
+				continue
+			}
+			nodeIdSize[prevNodeId] = nodeIdSize[nodeId] + 1
+			delete(nodeIdSize, nodeId)
+			// fmt.Printf("%v %v,%v\n", atBeginingCount, nodeIdSizeLength, prevNodeId)
+		}
+
+	}
+	fmt.Printf("node id size %v\n", nodeIdSize)
+	fmt.Printf("sequence size %v\n", sizeOfNewSequence)
+	// todo: The new sequence is a copy of 1 of the previous sequences.
+	// todo: The new and previous sequences have the same length, but number of matches < length of each sequence.
+	for nodeId := range nodeIdSize {
+		previousSequenceId := nodeId
+		for ; previousSequenceId != -1; previousSequenceId = (*sh.Sequences)[previousSequenceId].GetLastEdge("next") {
+			if newSequenceId, match := nodeIdMatches[previousSequenceId]; match {
+
+				newSequenceEdges := (*sh.Sequences)[newSequenceId].Edges
+				previousSequenceEdges := (*sh.Sequences)[previousSequenceId].Edges
+
+				newSequenceSize := sizeOfNewSequence
+				previousSequenceSize := nodeIdSize[nodeId]
+				if newSequenceSize > previousSequenceSize {
+					newSequenceEdges["parent"] = []int{previousSequenceId}
+					if _, isChildKeyPresent := previousSequenceEdges["child"]; !isChildKeyPresent {
+						previousSequenceEdges["child"] = []int{newSequenceId}
+					} else {
+						previousSequenceEdges["child"] = append(previousSequenceEdges["child"], newSequenceId)
+					}
+				} else if newSequenceSize < previousSequenceSize {
+					if _, isParentKeyPresent := previousSequenceEdges["parent"]; !isParentKeyPresent {
+						previousSequenceEdges["parent"] = []int{newSequenceId}
+					} else {
+						previousSequenceEdges["parent"] = append(newSequenceEdges["parent"], newSequenceId)
+					}
+					if _, isChildKeyPresent := newSequenceEdges["child"]; !isChildKeyPresent {
+						newSequenceEdges["child"] = []int{previousSequenceId}
+					} else {
+						newSequenceEdges["child"] = append(newSequenceEdges["child"], previousSequenceId)
+					}
+				}
 			}
 		}
 	}
@@ -361,7 +453,10 @@ func Pattern() {
 	fmt.Printf("\n")
 
 	sh.Categorize()
-
+	for _, item := range *sh.Sequences {
+		fmt.Printf("%v\n", item)
+	}
+	fmt.Printf("\n")
 	// checkFunctions := map[int][]string{}
 	// for _, item := range itemSequence1 {
 	// 	// fmt.Printf("%v. %v, %v\n", item, "check", strings.Contains(item, "check"))
